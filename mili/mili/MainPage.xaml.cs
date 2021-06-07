9cba7ad12e7af1765cb8146c46887fbab90b4789
Oleg21332 <60029491+Oleg21332@users.noreply.github.com>
2021-06-07 23:51:25 +0300

    Add files via upload

---
+++ mili/mili/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace mili
+
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public double kW(double с)
+        {
+            double k = i * 1.609;
+			return k;
+        }
+        public void Submit_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                double a = kW(Convert.ToDouble(First.Text));
+                double result = a;
+
+                Page1 secondpage = new Page1();
+                Navigation.PushAsync(secondpage);
+                secondpage.Calculator(result);
+            }
+            catch (FormatException)
+            {
+                DisplayAlert("Ошибка", "Ошибка введенных данных!", "Ввести заново");
+            }
+        }
+    }
+
+}
+
+
